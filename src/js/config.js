b56c172f2262096801b81d6c7b80f8044c978fbe
hishamcse <1805004@ugrad.cse.buet.ac.bd>
2021-05-11 14:37:45 +0600

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -2,5 +2,5 @@ export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
 export const TIMEOUT_SEC = 10;
 export const START_PAGE = 1;
 export const RESULTS_PER_PAGE = 10;
-export const API_KEY = '<Your API key>';              // please add your API_KEY here in order to run this project on your pc
+export const API_KEY = 'e596790d-d57a-46a6-98b3-a04ddf7803cc';              // please add your API_KEY here.API request is limited
 export const MODAL_CLOSE_TIME_SEC = 2.5;
